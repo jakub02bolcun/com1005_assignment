3c2b22755c1d027f19b4c0031d9d8d24f9061e3b
jakub <jakub@DESKTOP-IASGRME.home>
2021-05-19 15:10:36 +0100

    Implemented RamblersSearch.java

--- COM1005_Assignment_2021/code/search3/RamblersSearch.java
+++ COM1005_Assignment_2021/code/search3/RamblersSearch.java
@@ -1,4 +1,20 @@
 
 public class RamblersSearch extends Search {
 
+	private TerrainMap map; // the map that we will be searching
+    private int[][] goal; // the goal pixel
+    
+    public TerrainMap getMap() {
+    	return map;
+    }
+    
+    public int[][] getGoal() {
+        return goal;
+    }
+    
+    public RamblersSearch(TerrainMap m, int[][] g) {
+        map = m;
+        goal = g;    
+    }    
+	
 }
