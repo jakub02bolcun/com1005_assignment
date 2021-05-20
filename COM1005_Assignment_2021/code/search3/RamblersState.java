19885b7ce7a22228303123a2a6343fa2d4a9f377
jakub <jakub@DESKTOP-IASGRME.home>
2021-05-20 16:32:07 +0100

    Added remaining cost in constructor AND added corresponding getSuccessor method

--- COM1005_Assignment_2021/code/search3/RamblersState.java
+++ COM1005_Assignment_2021/code/search3/RamblersState.java
@@ -5,11 +5,19 @@ public class RamblersState extends SearchState {
 	// coordinate for current state
 	private Coords coordinate;
 	
-	// constructor
+	// constructor for BB
 	public RamblersState(Coords coordinates, int lc) {
 		coordinate = coordinates;
 		localCost = lc;
 	}
+	
+	// constructor for A*
+	public RamblersState(Coords coordinates, int lc, int rc) {
+		coordinate = coordinates;
+		localCost = lc;
+		estRemCost = rc;		
+	}
+	
 	
 	// accessor
 	public Coords getCoordinate() {
@@ -38,35 +46,48 @@ public class RamblersState extends SearchState {
 		int currenty = current.gety();
 		int maxX = map.getWidth();
 		int maxY = map.getDepth();
+		int remainingCost = estRemCost;
 		
 		// East successor
 		if (!(currentx+1 == maxX)) {
 			Coords successor = new Coords(currenty, currentx+1);
-			successors.add((SearchState) new RamblersState(successor, map.getTmap()[currenty][currentx+1]));
+			// BranchAndBound
+			//successors.add((SearchState) new RamblersState(successor, map.getTmap()[currenty][currentx+1]));
+			// A*
+			successors.add((SearchState) new RamblersState(successor, map.getTmap()[currenty][currentx+1], remainingCost));
 		}
 		
 		// West successor
 		if (!(currentx-1 == -1)) {
 			Coords successor = new Coords(currenty, currentx-1);
-			successors.add((SearchState) new RamblersState(successor, map.getTmap()[currenty][currentx-1]));
+			// BranchAndBound
+			//successors.add((SearchState) new RamblersState(successor, map.getTmap()[currenty][currentx-1]));
+			// A*
+			successors.add((SearchState) new RamblersState(successor, map.getTmap()[currenty][currentx-1], remainingCost));
 		}
 		
 		// North successor
 		if (!(currenty+1 == maxY)) {
 			Coords successor = new Coords(currenty+1, currentx);
-			successors.add((SearchState) new RamblersState(successor, map.getTmap()[currenty+1][currentx]));
+			// BranchAndBound
+			//successors.add((SearchState) new RamblersState(successor, map.getTmap()[currenty+1][currentx]));
+			// A*
+			successors.add((SearchState) new RamblersState(successor, map.getTmap()[currenty+1][currentx], remainingCost));
 		}
 		
 		// South successor
 		if (!(currenty-1 == -1)) {
 			Coords successor = new Coords(currenty-1, currentx);
-			successors.add((SearchState) new RamblersState(successor, map.getTmap()[currenty-1][currentx]));
+			// BranchAndBound
+			//successors.add((SearchState) new RamblersState(successor, map.getTmap()[currenty-1][currentx]));
+			// A*
+			successors.add((SearchState) new RamblersState(successor, map.getTmap()[currenty-1][currentx], remainingCost));
 		}
 		
 		return successors;
 	}
 
-	@Override // return true if same states
+	@Override // return true if same statess
 	public boolean sameState(SearchState s2) {
 		RamblersState rs2 = (RamblersState) s2;
 		//return (coordinate == rs2.getCoordinate());
