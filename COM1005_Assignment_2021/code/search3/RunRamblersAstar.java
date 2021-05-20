b948c44206427e8a34148336d9d2415145f3509f
jakub <jakub@DESKTOP-IASGRME.home>
2021-05-20 17:12:07 +0100

    Added Astar implmentation

--- COM1005_Assignment_2021/code/search3/RunRamblersAstar.java
+++ COM1005_Assignment_2021/code/search3/RunRamblersAstar.java
@@ -13,11 +13,13 @@ public class RunRamblersAstar {
 		TerrainMap map1 = new TerrainMap("code/search3/diablo.pgm");
 		Coords goalCoords = new Coords(goaly, goalx);
 		Coords initCoords = new Coords(inity, initx);
-		int remEstCost = map1.getTmap()[inity][initx];
+		int initHeight = map1.getTmap()[inity][initx];
+		int goalHeight = map1.getTmap()[goaly][goalx];
+		int remEstCost = goalHeight - initHeight;
+		System.out.println(remEstCost);
 		
 		RamblersSearch searcher = new RamblersSearch(map1, goalCoords);
-		SearchState initState = (SearchState) new RamblersState(initCoords, 0, remEstCost);
-		
+		SearchState initState = (SearchState) new RamblersState(initCoords, 0, remEstCost);		
 		
 		// show strings
 		//String res_bb = searcher.runSearch(initState, "AStar");
