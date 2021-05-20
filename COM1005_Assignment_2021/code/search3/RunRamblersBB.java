ae335091e086e9709da449db16c0e97e18015e1e
jakub <jakub@DESKTOP-IASGRME.home>
2021-05-20 14:15:50 +0100

    Added file with efficiency and random number generation for tests

--- COM1005_Assignment_2021/code/search3/RunRamblersBB.java
+++ COM1005_Assignment_2021/code/search3/RunRamblersBB.java
@@ -1,17 +1,24 @@
+import java.util.Random;
 
 public class RunRamblersBB {
 
 	public static void main(String[] arg) {
+		
+		Random rand = new Random();
+		int goalx = rand.nextInt(252);
+		int goaly = rand.nextInt(252);
+		int initx = rand.nextInt(252);
+		int inity = rand.nextInt(252);
+		
 		TerrainMap map1 = new TerrainMap("code/search3/diablo.pgm");
-		Coords goalCoords = new Coords(0, 0);
-		Coords initCoords = new Coords(43, 54);
+		Coords goalCoords = new Coords(goaly, goalx);
+		Coords initCoords = new Coords(inity, initx);
 		
 		RamblersSearch searcher = new RamblersSearch(map1, goalCoords);
 		SearchState initState = (SearchState) new RamblersState(initCoords, 0);
 		
 		float res_bb = searcher.runSearchE(initState, "branchAndBound");
 		System.out.println(res_bb);
-		
 		
 	}
 }
